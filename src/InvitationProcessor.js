5e1e04ec0851a1b28c43837166b3ab2b3c57f4a1
Mirla Chucre <mirlabraga@gmail.com>
2021-02-03 22:08:20 +0000

    implementation sort of customer for invitation and add test of it.

--- src/InvitationProcessor.js
+++ src/InvitationProcessor.js
@@ -6,24 +6,26 @@ const DISTANCE_FOR_INVITE = process.env.DISTANCE_FOR_INVITE || 100;
 async function execute(reporter, customers) {
 
   try {
-    customers.forEach(async customer => {
+    customers.sort((customer1, customer2) => customer1.user_id - customer2.user_id);
+
+    for (let i = 0; i < customers.length; i++) {
 
       const coordinates = {
-        longitude1: customer.longitude,
-        latitude1: customer.latitude,
+        longitude1: customers[i].longitude,
+        latitude1: customers[i].latitude,
         longitude2: POINT_OF_INTEREST.longitude,
         latitude2: POINT_OF_INTEREST.latitude
       };
 
       if (getGreatCircleDistance(coordinates) <= DISTANCE_FOR_INVITE) {
-        const result = await reporter.write({name: customer.name, user_id: customer.user_id});
+        const result = await reporter.write({name: customers[i].name, user_id: customers[i].user_id});
         console.info(`[INFO] ${result}`);
       }
-    })
+    }
   } catch (error) {
     throw new Error("an error happened when process the customers invitation.", error);
   }
-
 }
+
 
 exports.execute = execute;
--- tests/InvitationProcessor.test.js
+++ tests/InvitationProcessor.test.js
@@ -4,12 +4,12 @@ let report;
 
 beforeEach(() => {
   class ConsoleReport {
-    constructor(result) {
-      this.result = result;
+    constructor() {
+      this.result = [];
     }
 
     write(data) {
-      this.result = data;
+      this.result.push(data);
       return JSON.stringify(data);
     }
 
@@ -29,7 +29,7 @@ test('it checks whether the coordinate point is within a defined radius < 100)',
   ];
 
   await execute(report, customers);
-  expect(report.getResult()).toStrictEqual({ "name": "Ian Kehoe", "user_id": 4 });
+  expect(report.getResult()).toStrictEqual([{ "name": "Ian Kehoe", "user_id": 4 }]);
 
 });
 
@@ -40,7 +40,7 @@ test('it checks whether the coordinate point is within a defined radius > 100)',
   ];
 
   await execute(report, customers);
-  expect(report.getResult()).toStrictEqual(undefined);
+  expect(report.getResult()).toStrictEqual([]);
 
 });
 
@@ -51,16 +51,29 @@ test('it checks whether the coordinate point is equals the default coordinate (D
   ];
 
   await execute(report, customers);
-  expect(report.getResult()).toStrictEqual({ "name": "Bob Larkin", "user_id": 18 });
+  expect(report.getResult()).toStrictEqual([{ "name": "Bob Larkin", "user_id": 18 }]);
 
 });
 
 test('it checks when the process invitation throw a error', async () => {
   try {
-    const customers = {};
+    const customers = [];
     await execute(report, customers);
   } catch (error) {
     expect(error.message).toBe("an error happened when process the customers invitation.");
   }
+
+});
+
+test('checking order of the lists customers, should be sorted by user_id ascending.', async () => {
+
+  const customers = [
+    {"latitude": "53.0033946", "user_id": 39, "name": "Lisa Ahearn", "longitude": "-6.3877505"},
+    {"latitude": "53.1302756", "user_id": 5, "name": "Nora Dempsey", "longitude": "-6.2397222"},
+    {"latitude": "53.2451022", "user_id": 4, "name": "Ian Kehoe", "longitude": "-6.238335"}
+  ];
+
+  await execute(report, customers);
+  expect(report.getResult()).toStrictEqual([{"name": "Ian Kehoe", "user_id": 4}, {"name": "Nora Dempsey", "user_id": 5}, {"name": "Lisa Ahearn", "user_id": 39}]);
 
 });
